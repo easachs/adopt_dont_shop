2cf58bbd8ddbe9e3de3955a084da6e0f48433fea
Jared Hardinger <jaredhardinger@gmail.com>
2022-07-19 14:27:43 -0400

    Add action reqd instance variable

--- app/controllers/admin/shelters_controller.rb
+++ app/controllers/admin/shelters_controller.rb
@@ -10,5 +10,6 @@ class Admin::SheltersController < ApplicationController
     @avg_pet_age = shelter.avg_age
     @adoptable = shelter.num_adoptable
     @approved = shelter.num_approved
+    @action_reqd = shelter.action_required
   end
 end
