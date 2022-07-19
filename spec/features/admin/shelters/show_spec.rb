c61e79ebf4007df55a31d588c79bc8997a0f83fd
Jared Hardinger <jaredhardinger@gmail.com>
2022-07-18 21:24:14 -0400

    Remove space

--- spec/features/admin/shelters/show_spec.rb
+++ spec/features/admin/shelters/show_spec.rb
@@ -2,11 +2,6 @@ require 'rails_helper'
 
 RSpec.describe 'Admin Shelters Show Page' do
   it 'returns the shelter name and full address' do
-    # As a visitor
-    # When I visit an admin shelter show page
-    # Then I see that shelter's name and full address
-    #
-    # NOTE: Your query should only return the necessary data to complete the story
     shelter = Shelter.create(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9, address: '14 Maple St', zip: '81110')
 
     visit "/admin/shelters/#{shelter.id}"
