80345d4be91e3175dc70ae0c15ab3fd861f13b37
Eli Sachs <easachs13@gmail.com>
2022-07-19 15:16:18 -0600

    All Admin/User links

--- app/controllers/admin/shelters_controller.rb
+++ app/controllers/admin/shelters_controller.rb
@@ -5,11 +5,11 @@ class Admin::SheltersController < ApplicationController
   end
 
   def show
-    shelter = Shelter.find(params[:id])
+    @shelter = Shelter.find(params[:id])
     @shelter_address = Shelter.name_and_address(params[:id])
-    @avg_pet_age = shelter.avg_age
-    @adoptable = shelter.num_adoptable
-    @approved = shelter.num_approved
-    @action_reqd = shelter.action_required
+    @avg_pet_age = @shelter.avg_age
+    @adoptable = @shelter.num_adoptable
+    @approved = @shelter.num_approved
+    @action_reqd = @shelter.action_required
   end
 end
