99ef8b7094bba05edad83ed99910e7853dcbd67b
Jared Hardinger <jaredhardinger@gmail.com>
2022-07-18 22:14:25 -0400

    Create test for every shelter name linking to admin show page

--- spec/features/admin/shelters/index_spec.rb
+++ spec/features/admin/shelters/index_spec.rb
@@ -74,4 +74,29 @@ RSpec.describe 'Admin Shelters Index' do
       expect(page).to_not have_content(shelter_1.name)
     end
   end
+
+  # Admin Shelters Show Page Links
+  #
+  # As a visitor
+  # When I visit the admin shelter index ('/admin/shelters')
+  # Then I see that every shelter name is a link
+  # When I click one of these links
+  # Then I am taken to that shelter's admin show page
+
+  it 'makes every shelter name a link to the admin show page' do
+    shelter_1 = Shelter.create!(name: 'Boulder shelter', city: 'Boulder, CO', foster_program: false, rank: 9)
+    shelter_2 = Shelter.create!(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 8)
+    pet_1 = Pet.create!(adoptable: true, age: 1, breed: 'sphynx', name: 'Bare-y Manilow', shelter_id: shelter_1.id)
+    pet_2 = Pet.create!(adoptable: true, age: 3, breed: 'doberman', name: 'Lobster', shelter_id: shelter_2.id)
+    app_1 = App.create!(name: "Bob", address: "2020 Maple Lane", city: "Denver", state: "CO", zip: "80202", description: "ABC", status: "in progress")
+    app_2 = App.create!(name: "Jack", address: "2021 Maple Lane", city: "Denver", state: "CO", zip: "80202", description: "ABC", status: "pending")
+    PetApp.create!(pet: pet_1, app: app_1)
+    PetApp.create!(pet: pet_2, app: app_2)
+    visit "/admin/shelters"
+
+    expect(page).to have_link("#{@shelter_1.name}")
+    expect(page).to have_link("#{@shelter_2.name}")
+    click_link "#{shelter_1.name}"
+    expect(current_path).to eq("/admin/shelter/#{@shelter_1.id}")
+  end
 end
